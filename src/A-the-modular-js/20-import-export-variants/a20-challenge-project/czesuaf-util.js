e8885f361d28f7d07a083b5cf8020e8cc3c3e123
Jabi <michal.jabi@gmail.com>
2025-11-05 12:30:48 +0100

    refactor: ch(a20): make it right. (step2) + destructuring assigment

--- src/A-the-modular-js/20-import-export-variants/a20-challenge-project/czesuaf-util.js
+++ src/A-the-modular-js/20-import-export-variants/a20-challenge-project/czesuaf-util.js
@@ -1,18 +1,64 @@
-import { addNumbers, averageFrom } from './main/helpers/my-things.js'
-import { firstLetterToUpper } from './main/tools/tool-1.js'
-import { countWords } from './main/tools/tool-2.js'
-import someUtil, { toSecondPower } from './main/utils/some-util.js'
+import { addNumbers, averageFrom } from "./main/helpers/my-things.js";
+import { firstLetterToUpper as capitalize } from "./main/tools/tool-1.js";
+import { countWords } from "./main/tools/tool-2.js";
+import someUtil, { toSecondPower } from "./main/utils/some-util.js";
+
+//const lastLetter = someUtil.lastLetter;
+//const numberOfChars = someUtil.numberOfChars;
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring
+const { lastLetter, numberOfChars } = someUtil;
 
 export const stringHelper = {
-    lastLetter: someUtil.lastLetter,
-    numberOfChars: someUtil.numberOfChars,
-    countWords: countWords,
-    capitalize: firstLetterToUpper,
-}
-
+  lastLetter,
+  numberOfChars,
+  countWords,
+  capitalize,
+};
 
 export const numberHelper = {
-    toSecondPower: toSecondPower,
-    addNumbers: addNumbers,
-    averageFrom: averageFrom,
+  toSecondPower,
+  addNumbers,
+  averageFrom,
+};
+
+// Destrukturyzacja - przypadki użycia
+
+
+/*
+// Pseudokod react
+
+function useState(initial) {
+  function changeState(newState) {
+    initial = newState;
+  }
+
+  return [initial, changeState];
 }
+
+const [no, update] = useState(2);
+
+console.log(no);
+update(20)
+console.log(no)
+*/
+
+function toSecondPowerDesc(n) {
+  return [n, n ** 2];
+}
+
+const [x, xToSecond] = toSecondPowerDesc(3)
+
+console.log(x)
+console.log(xToSecond)
+
+console.log(toSecondPowerDesc(3))
+
+
+const myDate = '11/01/2025'
+
+const [m, d, y] = myDate.split('/');// .map(Number) | .map((x) => Number(x));
+
+console.log(m);
+console.log(d);
+console.log(y);
