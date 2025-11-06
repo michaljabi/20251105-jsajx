677527d65c838f3db678dce2ca348597f7279523
Jabi <michal.jabi@gmail.com>
2025-11-06 14:57:39 +0100

    refactor: wu(b50): make it right, explain setter and getter

--- src/B-objectivity/50-setters-getters/b50-1st-setters-getters-warm-up.js
+++ src/B-objectivity/50-setters-getters/b50-1st-setters-getters-warm-up.js
@@ -11,10 +11,28 @@ const person = {
 	// Kodzik można pisać tylko tutaj w środku.
 	name: 'Janusz',
 	lastName: 'Kowalski',
-	email() {
+	get email() {
 		return ([this.name, this.lastName].join('.') + '@workload.com').toLowerCase()
+	},
+	get fullName() {
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+		return [this.name, this.lastName ?? '-'].join(' ').trim()
+	},
+	set fullName(value) {
+		console.log(value);
+		console.log(value.split(' '));
+		const [name, lastName] = value.split(' ');
+		this.name = name;
+		this.lastName = lastName;
+		// this.fullName = value;
 	}
 }
+
+person.fullName = 'Janusz Kowalski'
+console.log(person.name)
+console.log(person.lastName)
+
+person.fullName //=
 
 // #Reguła:
 // Nie możesz zmieniać kodu poniżej:
@@ -24,12 +42,16 @@ assertThat(
 )  //=
 assertThat(
 	'Person email should be janusz.kowalski@workload.com',
-	expect => expect(person.email()).toBe('janusz.kowalski@workload.com')
+	expect => expect(person.email).toBe('janusz.kowalski@workload.com')
 )  //=
 // Po zmianie imienia i nazwiska powinien zmienić się email:
 person.name = 'Grażyna';
 person.lastName = 'Nowak';
 assertThat(
 	'Person email from now - should be grażyna.nowak@workload.com',
-	expect => expect(person.email()).toBe('grażyna.nowak@workload.com')
+	expect => expect(person.email).toBe('grażyna.nowak@workload.com')
 )  //=
+
+console.log(person.email);
+
+person.fullName //=
