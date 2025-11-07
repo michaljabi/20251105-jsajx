5c59976c7d9b3e1210290070a9fdd32ac74f1744
Jabi <michal.jabi@gmail.com>
2025-11-07 13:35:57 +0100

    explain: Private fields with #, shallow copy data to protect modification, getter to make tests works gain, removeGuest - mutation of the array.

--- src/D-functional-programming/40-generators-iterators/d40-3rd-generators-iterators-challenge.js
+++ src/D-functional-programming/40-generators-iterators/d40-3rd-generators-iterators-challenge.js
@@ -10,17 +10,26 @@ import { assertThat } from '../../j4b1-assert.js'
 
 class GuestList {
 
-	guests = [];
+	// pole prywatne (dostępne TYLKO w klasie, nie widoczne na instancji).
+	#myGuests = [];
 
 	signGuest(name, lastName) {
-		this.guests.push({name, lastName })
+		this.#myGuests.push({name, lastName })
 	}
 
-	helloList() {}
+	get guests() {
+		// robimy shallow copy SPECJALNIE, żeby nie udostępniać referencji do tablicy na zewnątrz
+		return [...this.#myGuests];
+	}
+
+	removeGuest(idx) {
+		// metoda mutująca tablice z danymi.
+		this.#myGuests.splice(idx, 1);
+	}
 
 	// Jeśli wrzucisz instancję GuestList do pętli for, to wtedy zobaczysz imiona.
 	*[Symbol.iterator]() {
-		for(const guest of this.guests) {
+		for(const guest of this.#myGuests) {
 			yield guest.name
 		}
 	}
@@ -33,6 +42,10 @@ const myGuests = new GuestList();
 console.log(myGuests)
 console.log(myGuests.guests)
 
+// to nie zadziała teraz (i o to nam chodziło)
+// ponieważ getter nie daje referencji do tablicy #myGuests [!]
+myGuests.guests.push({name: 'Troll'})
+
 // Przepis na sprawdzenie propery, methods i "symbols" na intancji klasy
 console.log(Object.getOwnPropertyNames(myGuests));
 console.log(Object.getOwnPropertyNames(myGuests.constructor.prototype));
@@ -43,6 +56,7 @@ myGuests.signGuest('Jane', 'Doe');
 myGuests.signGuest('Joe', 'Doe');
 myGuests.signGuest('Jan', 'Doe');
 myGuests.signGuest('Janina', 'Doe');
+
 
 const collector = [];
 // odkomentuj poniższy blok, kiedy będzie już implementacja:
