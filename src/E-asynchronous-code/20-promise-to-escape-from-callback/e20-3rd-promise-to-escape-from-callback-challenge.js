8f77802ec58367ca1f4ea85c6130362749bc0107
Jabi <michal.jabi@gmail.com>
2025-11-07 11:00:50 +0100

    refactor: ch(e20): make it better - another 2 solutions, full API - async/await syntax

--- src/E-asynchronous-code/20-promise-to-escape-from-callback/e20-3rd-promise-to-escape-from-callback-challenge.js
+++ src/E-asynchronous-code/20-promise-to-escape-from-callback/e20-3rd-promise-to-escape-from-callback-challenge.js
@@ -14,13 +14,34 @@ import { assertThat } from '../../j4b1-assert.js'
 
 
 const DZej = {
-	getJSON(url) {
+	/*getJSON(url) {
 		// #Reguła:
 		// Kodzik można pisać i zmieniać tylko w tym bloku.
 		if(url.endsWith('/it')) {
 			return Promise.resolve({ message: 'did you try?' })
 		}
 		return Promise.reject({status: 404, message: 'Invalid URL!'})
+	},
+	// pełne API (sposób 2)
+	getJSON(url) {
+		return new Promise((resolve, reject) => {
+			if(url.endsWith('/it')) {
+				resolve({ message: 'did you try?' })
+			} else {
+				reject({status: 404, message: 'Invalid URL!'})
+			}
+		})
+	},
+	*/
+	// po 2017: async/await
+	async getJSON(url) {
+		if(!url.endsWith('/it')) {
+			// docelowo - staraj się używać: new Error('Invalid URL!')
+			// w JS można rzucić (throw) - dowolną rzecz, ale NIE JEST TO DOBRY POMYSŁ
+			// zawsze staraj się rzucić Error! albo coś co dziedziczy po Error
+		   throw {status: 404, message: 'Invalid URL!'}	
+		}
+		return { message: 'did you try?' }
 	}
 }
 
