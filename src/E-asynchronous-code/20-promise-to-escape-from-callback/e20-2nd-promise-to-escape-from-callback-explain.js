15bb83bb880e013f414de257a71d40b1eb138da4
Jabi <michal.jabi@gmail.com>
2025-11-07 10:22:24 +0100

    explain(e20): more about Promise

--- src/E-asynchronous-code/20-promise-to-escape-from-callback/e20-2nd-promise-to-escape-from-callback-explain.js
+++ src/E-asynchronous-code/20-promise-to-escape-from-callback/e20-2nd-promise-to-escape-from-callback-explain.js
@@ -14,17 +14,23 @@
 
 // Przypomnijmy sobie o callbacku:
 
+// PROVIDER
 function makeMeACoffee(serveCoffee) {
-	// wait 200ms
-	setTimeout(() => {
-		serveCoffee('Mocha')
-	}, 200)
+  // wait 200ms
+  setTimeout(() => {
+    serveCoffee("Mocha");
+    serveCoffee("Espresso");
+  }, 200);
 }
 
+// CONSUMER 1
 makeMeACoffee((myCoffee) => {
-	console.log(myCoffee)
-})
-
+  console.log(myCoffee);
+});
+// CONSUMER 2
+makeMeACoffee((myCoffee) => {
+  console.log(myCoffee);
+});
 
 // W tym układzie - to tzw. "Wesoła ścieżka" (Happy path).
 // 1) wrzucam callback
@@ -37,37 +43,36 @@ makeMeACoffee((myCoffee) => {
 // a) Zawsze może się wykonać (np. dla event DOM - click w button)
 // b) Nie następuje po sobie klika operacji asynchronicznych / synchronicznych - jedna po drugiej (gdzie są od siebie zależne)
 
-
 // Próba zastosowania callback'ów do operacji asynchronicznych, które mogą się wykonać - lub zwrócić error,
 // doprowadza nas do takich konstrukcji:
 
 function makeAjaxCall(url, callbackFn) {
-	// Albo:
-	callbackFn('resolved', null);
-	// Albo:
-	// callbackFn(null, new Error('reject'));
+  // Albo:
+  callbackFn("resolved", null);
+  // Albo:
+  // callbackFn(null, new Error('reject'));
 }
 
 // Przykład callback hell:
-makeAjaxCall('https://first', (data, err) => {
-	if(err) {
-		console.log('error 😐', err)
-		return;
-	}
-	makeAjaxCall('https://second' + data.url, (data, err) => {
-		if(err) {
-			console.log('error 😐', err)
-			return;
-		}
-		makeAjaxCall('https://third' + data.url, (data, err) => {
-			if(err) {
-				console.log('error 😐', err)
-				return;
-			}
-			console.log('THIS IS MADNESS !!!')
-		})
-	})
-})
+makeAjaxCall("https://first", (data, err) => {
+  if (err) {
+    console.log("error 😐", err);
+    return;
+  }
+  makeAjaxCall("https://second" + data.url, (data, err) => {
+    if (err) {
+      console.log("error 😐", err);
+      return;
+    }
+    makeAjaxCall("https://third" + data.url, (data, err) => {
+      if (err) {
+        console.log("error 😐", err);
+        return;
+      }
+      console.log("THIS IS MADNESS !!!");
+    });
+  });
+});
 
 // Promise - TO THE RESCUE!
 // Czy można rozwiązać to prościej - tak.
@@ -78,53 +83,75 @@ makeAjaxCall('https://first', (data, err) => {
 
 // Zobaczmy najprostrzy przykład wywołania:
 
-const provider = Promise.resolve('hello');
+const provider = Promise.resolve("hello");
 
 // Odbiór promise:
 // CONSUMER:
 provider.then((message) => {
-	console.log(message)
-})
+  console.log(message);
+});
 
 // całe piękno polega na tej własności:
 provider
-	.then((message) => {
-		console.log(message)
-		return 1234;
-	})
-	.then((num) => {
-		console.log(num);
-		return Promise.resolve('other promise');
-	})
-	.then((msg) => {
-		console.log(msg);
-	})
+  .then((message) => {
+    console.log(message);
+    throw new Error("oh no...");
+    return 1234;
+  })
+  .then((num) => {
+    console.log(num);
+    // throw new Error('oh no...')
+    return Promise.resolve("other promise");
+  })
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(() => {
+    console.log("I will always fire");
+  });
 
 // możemy "chainować" wywołania then:
 // Promise.resolve().then().then().then()
 
 // zobacz jak upraszcza nam to przykład z callback hell:
 
+// const array: Array<string | number> = [1, '2'];
+
 function makeAjaxCallAsPromise(url) {
-	// Albo:
-	return Promise.resolve('resolved');
-	// Albo:
-	// return Promise.reject(new Error('reject'));
+  // Albo:
+  // return Promise.resolve("resolved");
+  // Albo:
+  // return Promise.reject(new Error('reject'));
+  // Albo pełne API:
+  // PROVIDER
+  return new Promise((resolve, reject) => {
+    //resolve("resolved")
+    setTimeout(() => {
+      if (url === "") {
+        reject("reject");
+      } else {
+        resolve("resolved");
+      }
+    }, 2000);
+  });
 }
 
-makeAjaxCallAsPromise('https://first')
-	.then((data) => {
-		return makeAjaxCallAsPromise('https://second' + data.url);
-	})
-	.then((data) => {
-		return makeAjaxCallAsPromise('https://third' + data.url);
-	})
-	.then((data) => {
-		console.log('THIS IS COOLNESS !!!', data);
-	})
-	.catch((err) => {
-		console.log('error 😐', err)
-	})
+makeAjaxCallAsPromise("https://first")
+  .then((url) => {
+    return makeAjaxCallAsPromise("https://second" + url);
+  })
+  .then((url) => {
+    return makeAjaxCallAsPromise("https://third" + url);
+  })
+  .then((data) => {
+    console.log("THIS IS COOLNESS !!!", data);
+  })
+  .catch((err) => {
+    console.log("error 😐", err.message);
+  });
 
 // Z promise - mamy 2 opcje:
 
@@ -137,34 +164,33 @@ makeAjaxCallAsPromise('https://first')
 // Pełne API do utworzenia Promise, wygląda następująco:
 
 const promiseProvider = new Promise((resolve, reject) => {
-	// zrób coś synchronicznie, lub asynchronicznie
-	// ponieważ mamy dostęp do resolve i reject jako callbacks:
-	setTimeout(() => {
-		resolve('OK')
-		// lub:
-		// reject(new Error('No way !'))
-	}, 200)
-})
-
+  // zrób coś synchronicznie, lub asynchronicznie
+  // ponieważ mamy dostęp do resolve i reject jako callbacks:
+  setTimeout(() => {
+    resolve("OK");
+    // lub:
+    // reject(new Error('No way !'))
+  }, 200);
+});
 
 // Zwróć uwagę, że powyższy zapis może być resolved - natychmiastowo (synchronicznie), lub asynchronicznie
 // Dla uproszczenia - Promise posiada 2 metody statyczne, jeśli chcemy mieć Promise, która natychmiastowo jest resolved lub rejected.
 // Zobacz jak to działa w praktyce.
 
 // Zamiast pisać:
-new Promise((resolve => {
-	resolve('Hello')
-}))
+new Promise((resolve) => {
+  resolve("Hello");
+});
 // można w skrócie napisać:
-Promise.resolve('Hello')
+Promise.resolve("Hello");
 
 // Podobnie z REJECT:
 // Zamiast pisać:
-new Promise(((resolve, reject) => {
-	reject('Oh no !')
-}))
+new Promise((resolve, reject) => {
+  reject("Oh no !");
+});
 // można w skrócie napisać:
-Promise.reject('Oh no !')
+Promise.reject("Oh no !");
 
 // TO w połączeniu z tym że możemy chain'ować `.then()` i w kolejnych wywołaniach `.then()`
 // podawać dane albo w postaci kolejnych Promise - albo dowolnych innych obiektów, które i tak zostaną opakowane,
